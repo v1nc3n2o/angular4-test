823016d3005f12eab57d8c8827d945cc2e8abf90
Vincenzo Viola <vinc.viola@gmail.com>
2017-10-29 18:13:49 +0100

    added custom themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  theme: string;
+
+  constructor() { }
+
+  public ngOnInit(): void {
+    this.theme = 'my-theme-one';
+    Observable.interval(1000 * 3).subscribe(x => {
+      this.doSomething();
+    });
+  }
+
+  doSomething() {
+    if (this.theme === 'my-theme-one') {
+      this.theme = 'my-theme-two';
+    } else {
+      this.theme = 'my-theme-one';
+    }
+  }
 }
